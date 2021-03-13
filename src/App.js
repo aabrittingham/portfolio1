da17d11331ca7db6747c0d3ac61ae6a457a33fdf
aabrittingham <aabrittingham@users.noreply.github.com>
2021-03-12 22:29:22 -0500

    portfolio 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Run from "./components/Run";
+import JumpRope from "./components/JumpRope";
+import Weights from "./components/Weights";
+import Swimming from "./components/Swimming";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Exercise Tracker</h1>
+      <Run />
+      <JumpRope />
+      <Weights />
+      <Swimming />
     </div>
   );
 }
